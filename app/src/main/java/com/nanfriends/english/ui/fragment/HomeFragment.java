9b16f4d9944d8b47ae83e06b5082f231663014cd
倚窗听雨211 <956603199@qq.com>
2018-12-03 11:34:37 +0800

    添加viewpager切换

--- app/src/main/java/com/nanfriends/english/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/nanfriends/english/ui/fragment/HomeFragment.java
@@ -1,24 +1,40 @@
 package com.nanfriends.english.ui.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.content.Intent;
 import android.view.View;
-import android.widget.TextView;
 
+import com.chenchen.collections.utils.SPUtils;
 import com.nanfriends.english.R;
+import com.nanfriends.english.ui.ListActivity;
+import com.nanfriends.english.ui.LoginActivity;
 
 import org.xutils.view.annotation.ContentView;
-import org.xutils.view.annotation.ViewInject;
+import org.xutils.view.annotation.Event;
 
 @ContentView(R.layout.fragment_home)
 public class HomeFragment extends BaseFragment {
-    @ViewInject(R.id.home_text)
-    TextView textView;
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        //TODO 这里写fragment中要做的事
-        textView.setText("这是首页");
+    @Event({R.id.listen,R.id.reader,R.id.translate,R.id.writer,R.id.logout})
+    private void onClick(View view){
+        Intent intent = new Intent(getActivity(),ListActivity.class);
+        switch (view.getId()){
+            case R.id.listen:
+                intent.putExtra("tx",1);
+                break;
+            case R.id.reader:
+                intent.putExtra("tx",2);
+                break;
+            case R.id.translate:
+                intent.putExtra("tx",3);
+                break;
+            case R.id.writer:
+                intent.putExtra("tx",4);
+                break;
+            case R.id.logout:
+                SPUtils.getInstance(getActivity(),"english").clear();
+                changeActivity(LoginActivity.class,true);
+                return;
+        }
+        changeActivity(intent,false);
     }
 }
